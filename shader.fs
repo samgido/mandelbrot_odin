ec0e11a4c3d247848809df8afb910b05accc3f55
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-02-18 21:59:41 -0600

    color via iteration and comments'

--- shader.fs
+++ shader.fs
@@ -17,30 +17,31 @@ void main()
 
     bool in_mandelbrot = true;
 
-    int max_iter = 1000;
-    int iter = 0;
-    while (iter < max_iter)
+    int max_iter = 700;
+    int iter;
+    for (iter = 0; iter < max_iter; iter += 1)
     {
         float tx = z.x;
         float ty = z.y;
 
+        // 'square' the complex number z
         z.x = tx*tx - ty*ty;
         z.y = 2 * tx * ty;
 
+        // add onto it the constant c, which is, in this case, the position of the pixel we are calculating for
         z.x += uv.x;
         z.y += uv.y;
         
-        if (z.x * z.x + z.y * z.y > 4.0)
+        // If the distance from the origin is less than 2
+        if (z.x * z.x + z.y * z.y > 6.0)
         {
             in_mandelbrot = false;
             break;
         }
-
-        iter += 1;
     }
 
     if (in_mandelbrot)
-        finalColor = vec4(1,1,1,1);
-    else
         finalColor = vec4(0,0,0,1);
+    else
+        finalColor = vec4(50 * (float(iter) / float(max_iter)),0,0,1);
 }
